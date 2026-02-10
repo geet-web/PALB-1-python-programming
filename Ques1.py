d96fd0ba748ed99598107a30d14256a6323074bc
geet-web <geetanshisingh11@gmail.com>
2026-02-10 11:42:28 +0530

    Fix comment formatting in Ques1.py

---
+++ Ques1.py
@@ -0,0 +1,6 @@
+//You are given an array of integers arr[]. You have to reverse the given array.
+class Solution:
+    def reverseArray(self, arr):
+        n = len(arr)
+        for i in range(n // 2):
+            arr[i], arr[n - i - 1] = arr[n - i - 1], arr[i]
