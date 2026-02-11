7e37091532e90c4fa287dfac744b8debd3bb1929
geet-web <geetanshisingh11@gmail.com>
2026-02-11 21:47:51 +0530

    Fix comment formatting in Ques8.py

--- Ques8.py
+++ Ques8.py
@@ -1,5 +1,5 @@
 #Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
-You must write an algorithm with O(log n) runtime complexity.
+#You must write an algorithm with O(log n) runtime complexity.
 class Solution:
     def searchInsert(self, nums: list[int], target: int) -> int:
         left = 0
